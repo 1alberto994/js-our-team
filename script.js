997d60786361501e5652f2a667f31f7a6d347b4d
1alberto994 <alby94ct@gmail.com>
2023-01-16 13:16:24 +0100

    Milestone 0

---
+++ script.js
@@ -0,0 +1,33 @@
+const dati=[
+    {
+        nome:"Wayne Barnett",
+        role:"Founder e CEO",
+        image:"wayne-barnett-fonder-ceo.jpg",
+    },
+    {
+        nome:"Angela Caroll",
+        role:"Chief editor",
+        image:"angela-caroll-chief-editor.jpg",
+    },
+    {
+        nome:"Walter Gordon",
+        role:"Office Manager",
+        image:"walter-gordon-office-manager.jpg",
+    },
+    {
+        nome:"Angela Lopez",
+        role:"Social media manager",
+        image:"angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome:"Scott Estrada",
+        role:"Developer",
+        image:"scott-estrada-developer.jpg",
+    },
+    {
+        nome:"Barbara Ramos",
+        role:"Graphic Designer",
+        image:"barbara-ramos-graphic-designer.jpg",
+    },
+]
+console.log(dati)
